9cb0e1964b576149ebbe3f5acfdc4b4598406ff4
Ludvig Vigerust <l.vigerust@gmail.com>
2023-11-14 20:22:52 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <div class="min-h-screen grid place-content-center">
+	<h1 class="font-Display text-4xl font-semibold">Hei fra Develop branch</h1>
 	<h1 class="font-Display text-4xl font-semibold">Coming soon.</h1>
 </div>
